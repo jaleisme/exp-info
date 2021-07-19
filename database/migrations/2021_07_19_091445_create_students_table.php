8b7af237109b0f848182989124060a162f00a3fe
Faizal Ramadhan <jaleisme.id@gmail.com>
2021-07-19 16:34:33 +0700

    Base Auth Done

---
+++ database/migrations/2021_07_19_091445_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('student_uid');
+            $table->string('photo');
+            $table->string('address');
+            $table->string('tel_num');
+            $table->string('pob');
+            $table->string('dob');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
